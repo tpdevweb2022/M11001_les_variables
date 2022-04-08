868f28ef8d2af8485699cf479bef25e67d9b71f9
anthonymerlier <webmaster@consor.fr>
2022-04-08 15:55:42 +0200

    Chapter 1

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+/* EXERCICE 1 */
+    // Déclarer une variable nommée $text qui contient le texte « Bonjour »
+
+    // Déclarer une autre variable, qui contiendra « à tout le monde les gens ! »
+
+    // Concaténer les deux variables
+
+
+
+/* EXERCICE 2 */
+    // Puis une variable $number1 égale à 124 et une variable $number2 qui vaut 378.24
+
+    // Effectuer l’addition de $number1 et $number2
+
+
+
+/* EXERCISE 3 */
+    // Corriger la déclaration de la variable suivante avec la méthode de votre choix
+    // $exercice3 = " On l'appelle "Trinita" ";
+
+
+/* EXERCISE 4 */
