8a11a504f4d3c8acae8627b93a72a15769178fba
anthonymerlier <webmaster@consor.fr>
2022-04-08 16:15:24 +0200

    Ex2 ok

---
+++ M11001-2.php
@@ -0,0 +1,6 @@
+<?php
+
+// Créer une constante avec la fonction adaptée, et lui donner le nom et la valeur 18
+
+
+// Utiliser cette constante dans une nouvelle variable de type string, ayant pour contenu "Le numéro des pompiers et le " puis concaténer votre constante
